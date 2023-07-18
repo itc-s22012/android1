7d0750275bac1c60e9da4d4a42989f03f5384eff
s22012 <s22012@std.it-college.ac.jp>
2023-07-18 14:03:40 +0900

    chapter4終わり。ViewBindingをつかうように変更した。

--- HelloSample/app/src/main/java/jp/ac/it_college/std/s22012/hellosample/MainActivity.kt
+++ HelloSample/app/src/main/java/jp/ac/it_college/std/s22012/hellosample/MainActivity.kt
@@ -6,24 +6,27 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import jp.ac.it_college.std.s22012.hellosample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val btClick = findViewById<Button>(R.id.btClick)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+//ボタンはとってくる
+        //val btClick = findViewById<Button>(R.id.btClick)
         val listener = HelloListener()
-        btClick.setOnClickListener(listener)
+        binding.btClick.setOnClickListener(listener)
+        //btClick.setOnClickListener(listener)
     }
 
     private inner class HelloListener : View.OnClickListener {
         override fun onClick(v: View?) {
-            val input = findViewById<EditText>(R.id.etName)
-            val output = findViewById<TextView>(R.id.tvOutput)
-            val inputStr = input.text.toString()
-            output.text = "${inputStr}さんこんにちは"
+            // val input = binding.etName
+           // val output = binding.tvOutput
+            val inputStr = binding.etName.text.toString()
+            binding.tvOutput.text = "${inputStr}さんこんにちは"
         }
 
     }
