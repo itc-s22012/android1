0125b28cf74d6ab1aecd62ba5d17c80bcfb4b3fb
s22012 <s22012@std.it-college.ac.jp>
2023-08-15 17:01:35 +0900

    9.4

--- FragmentSample/app/src/main/java/jp/ac/it_college/std/s22012/fragmentsample/MenuListFragment.kt
+++ FragmentSample/app/src/main/java/jp/ac/it_college/std/s22012/fragmentsample/MenuListFragment.kt
@@ -5,10 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import jp.ac.it_college.std.s22012.fragmentsample.databinding.FragmentMenuListBinding
-
+internal const val REQUEST_SELECTED_MENU = "resultMenu"
+internal const val RESULT_NAME = "menuName"
+internal const val RESULT_PRIME = "menuPrice"
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 
@@ -36,7 +39,13 @@ class MenuListFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         binding.menuList.apply {
-            adapter = MenuAdapter(teishokuList)
+            adapter = MenuAdapter(teishokuList) {
+                parentFragmentManager.setFragmentResult(REQUEST_SELECTED_MENU, bundleOf(
+                    RESULT_NAME to it.name,
+                    RESULT_PRIME to it.price
+                )
+                )
+            }
             val manager = LinearLayoutManager(context)
             layoutManager = manager
             addItemDecoration(DividerItemDecoration(context,manager.orientation))
